d2a33b6e2188e48ca7438d393bf62b4edad9ad02
laurence <laurence.freeman@protonmail.com>
2022-01-12 16:17:43 +0000

    Spectrogram and PSD fuctions commited with band pass filter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import reshape
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 
 #Load and shape data
 file = "/Users/freeman/Documents/saleem_lab/data/R21011_210915_CA1_1.dat"
@@ -8,11 +9,26 @@ data_object = reshape.Import_and_Shape_Data(file)
 data = data_object.reshape_data()
 
 #Parameters
-sample_rate = 20000
+fs = 20000
 block_size = len(data[0])
-frame_size = block_size / sample_rate
+frame_size = block_size / fs
+c1 = data[0]
+band_pass_range = [1,300]
 
-#Fourier trasnform
-transform = np.fft.fft(data[0])
-plt.plot(transform)
+#Bandpass Filter
+#How to choose the best order / N?
+#What does SOS truely mean?
+sos = signal.butter(N = 10, Wn = band_pass_range, btype = 'bandpass', output = 'sos', fs = fs)
+filtered = signal.sosfilt(sos, c1)
+
+#Plots
+fig, (psd, spectrogram) = plt.subplots(2, 1)
+fig.suptitle("Channel 1, PSD + Spectrogram. Bandpass filter range {} Hz".format(band_pass_range))
+Pxx, freqs = psd.psd(filtered, Fs = fs, scale_by_freq = False) #Power spectrum
+psd.set_ylim([0, 80])
+psd.set_xlim(band_pass_range)
+spectrum, freqs, t, im = spectrogram.specgram(filtered, Fs = fs, scale_by_freq = False) #Spectrogram
+spectrogram.set_ylim(band_pass_range)
+spectrogram.set_xlabel('Time (Seconds)')
+spectrogram.set_ylabel('Frequency (Hz)')
 plt.show()
