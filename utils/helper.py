da6f96293c85f0c52aacde58675ff69642f07a72
laurence <laurence.freeman@protonmail.com>
2022-01-25 12:02:15 +0000

    Adding unfinished tetrode conversion code

--- utils/helper.py
+++ utils/helper.py
@@ -5,3 +5,15 @@ def dB(x, out=None):
     else:
         np.log10(x, out)
         np.multiply(out, 10, out) #Convert power into decibals which is a log scale
+
+# #Convert 64 channels into 16 tetrodes - convert into function - unfinished code not needed for tomazzo data as silicon probes have a different geometry to normal tetrodes
+# tetrode_data = {}
+# counter = 1
+# for i in range(16):
+#     if i == 0:
+#         tetrode_data[i] = np.mean(data[:,    0:4], axis = 1)
+#     else:
+#         tetrode_data[i] = np.mean(data[:,    4*counter:4* (counter+1)], axis = 1)
+#         counter += 1
+# # print(tetrode_data.shape)
+# print('Length of averaged channels', len(tetrode_data[0]))
