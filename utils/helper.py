42c82f18f3da9dcf29f3df84f95e4e290e2e32f5
laurence <laurence.freeman@protonmail.com>
2022-01-31 12:25:09 +0000

    adding a function to downsample

--- utils/helper.py
+++ utils/helper.py
@@ -1,10 +1,16 @@
 import numpy as np
+
 def dB(x, out=None):
     if out is None:
         return 10 * np.log10(x)
     else:
         np.log10(x, out)
         np.multiply(out, 10, out) #Convert power into decibals which is a log scale
+
+def downsample(data, original_fs, desired_fs):
+    down_sampling_factor = int(original_fs/ desired_fs)
+    down_sampled_data    = data[0:-1:down_sampling_factor, :]
+    return down_sampled_data
 
 # #Convert 64 channels into 16 tetrodes - convert into function - unfinished code not needed for tomazzo data as silicon probes have a different geometry to normal tetrodes
 # tetrode_data = {}
