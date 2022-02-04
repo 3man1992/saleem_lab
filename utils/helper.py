0bd81e6d1ec2025b43b998222605c8f45ab16771
laurence <laurence.freeman@protonmail.com>
2022-02-04 14:57:31 +0000

    refactor

--- utils/helper.py
+++ utils/helper.py
@@ -12,15 +12,3 @@ def downsample(data, original_fs, desired_fs):
     down_sampled_data    = data[0:-1:down_sampling_factor, :]
     num_of_samples = len(data[0:-1:down_sampling_factor, 0])
     return down_sampled_data, num_of_samples
-
-# #Convert 64 channels into 16 tetrodes - convert into function - unfinished code not needed for tomazzo data as silicon probes have a different geometry to normal tetrodes
-# tetrode_data = {}
-# counter = 1
-# for i in range(16):
-#     if i == 0:
-#         tetrode_data[i] = np.mean(data[:,    0:4], axis = 1)
-#     else:
-#         tetrode_data[i] = np.mean(data[:,    4*counter:4* (counter+1)], axis = 1)
-#         counter += 1
-# # print(tetrode_data.shape)
-# print('Length of averaged channels', len(tetrode_data[0]))
